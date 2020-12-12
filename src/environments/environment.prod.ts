b3bb56365b95bed8b0b6189b759b900e1d70bb52
Claudio Junior Familia Morel <cfamilia@mardom.com>
2020-12-12 00:15:45 -0400

    Fix build errors

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,19 @@
 export const environment = {
   production: true
 };
+
+export const config: any = {
+	ApiUrl: 'https://incidentapp-api.herokuapp.com/api'
+};
+
+export class AppSettings {
+  getModalBasicConf(): any {
+    return {
+      size: 'lg',
+      backdrop: 'static',
+      keyboard: false,
+      windowClass: 'animated slideInDown',
+      centered: true,
+    };
+  }
+}
