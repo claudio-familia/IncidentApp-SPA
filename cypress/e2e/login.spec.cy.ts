9533023f31d29ecbe80cf6801f85374ccefc682c
Claudio Familia Morel <claudio.familia.morel@gmail.com>
2022-06-07 13:39:47 -0400

    test(login-component): add testing suit

---
+++ cypress/e2e/login.spec.cy.ts
@@ -0,0 +1,37 @@
+/// <reference types="cypress" />
+
+describe('Login process', () => {
+  const validUser = {
+    username: 'Administrator',
+    password: '1234'
+  }
+
+  beforeEach(() => {
+    cy.visit('/');
+    cy.contains('Incident App');
+  });
+
+  it('TryLoginWithCorrectUser', () => {
+    cy.get('input[name=username]').type(validUser.username);
+    cy.get('input[name=password]').type(validUser.password);
+    cy.get('button.btn-login').click();    
+    setTimeout(() => {}, 3000);
+    cy.contains('Usuarios')
+  });
+
+  it('TryLoginWithNotExistingUser', () => {
+    cy.get('input[name=username]').type("random user");
+    cy.get('input[name=password]').type(validUser.password);
+    cy.get('button.btn-login').click();  
+    setTimeout(() => {}, 3000);
+    cy.contains('Error al validar')
+  });
+
+  it('TryLoginWithInvalidCredentials', () => {
+    cy.get('input[name=username]').type(validUser.username);
+    cy.get('input[name=password]').type("123456789");
+    cy.get('button.btn-login').click();
+    setTimeout(() => {}, 3000);
+    cy.contains('Error al validar')
+  });
+})
